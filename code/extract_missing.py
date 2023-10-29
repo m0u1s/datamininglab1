768fb9d4ca047d109f86ae274f02ead11ff359f7
m0u1s <93197852+m0u1s@users.noreply.github.com>
2023-10-29 15:45:59 +0700

    mo ngu ngu

--- code/extract_missing.py
+++ code/extract_missing.py
@@ -14,29 +14,32 @@ def extract_missing(matrix: list, attribute: list):
     num_cols= len(matrix[0])
     missing = []
     #track and mark the cols with missing value
-    for i in range(0, num_rows):
-        for j in range(0, num_cols):
+    for j in range(0, num_cols):
+        for i in range(0, num_rows):
             if(matrix[i][j] == ''):
                 missing.append(j)
-                continue
+                break
     #Print out
     return missing
+
 def missing_columns_matrix(matrix: list, missing: list, atr: list):
     missing_matrix = []
-    n = len(matrix)
-    index= 0 
+    n = len(matrix) 
     fields = []
     for _ in missing:
         fields.append(atr[_])
-        for i in range(0, n):
-            missing_matrix[i][index] = matrix[i][_]
-        index += 1 
+    for i in range(0, n):
+        row = []
+        for _ in missing:
+            row.append(matrix[i][_])
+        missing_matrix.append(row)
     return missing_matrix, fields
 
 #code run
 missing= extract_missing(readFile.myMatrix, readFile.at)
 matrix, fields= missing_columns_matrix(readFile.myMatrix, missing, readFile.at)
 
+print(len(matrix))
 output.outputFile(matrix, fields)  
 
 
--- code/output.py
+++ code/output.py
@@ -1,8 +1,10 @@
 import csv
 import command_line
 def outputFile(matrix: list, fields: list):  
-    with open(command_line.output, 'w') as f:
+    with open(command_line.args.output, 'w', newline='') as f:
     # using csv.writer method from CSV package
         out = csv.writer(f)
         out.writerow(fields)
-        out.writerows(matrix)
+        for row in matrix:
+            out.writerow(row)
+    print('done')
